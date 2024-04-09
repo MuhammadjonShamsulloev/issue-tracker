2c2d0e346c5142abf6ea9e7960217622c6209845
Muhammadjon <shamsulloevmuhammadjon@gmail.com>
2024-04-08 23:15:46 -0400

    Add the edit button

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,12 +1,10 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import { Heading, Flex, Text, Box, Card } from '@radix-ui/themes';
-import ReactMarkdown from 'react-markdown';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import { Flex, Box, Card } from '@radix-ui/themes';
+import { Skeleton } from '@/app/components';
 
 const LoadingIssueDetailPage = () => {
   return (
-    <Box className='max-w-xl'>
+    <Box className="max-w-xl">
       <Skeleton />
       <Flex className="space-x-3" my="2">
         <Skeleton width="5rem" />
